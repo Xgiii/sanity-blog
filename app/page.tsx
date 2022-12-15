b8b3fc8c397974bccc8c7f859c23cbce50f8ee15
Filip Gurak <filip.gurak999@gmail.com>
2022-12-15 17:02:59 +0100

    merged sanity

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Page() {
+  return (
+    <div>
+        <h1 className='text-3xl'>Welcome to the Blog</h1>
+        <p>Some content</p>
+    </div>
+  )
+}
+
+export default Page
