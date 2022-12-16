fc11bef428f2de379064fd2a734afb68734421f4
Filip Gurak <filip.gurak999@gmail.com>
2022-12-16 15:19:09 +0100

    studio theme

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,12 +2,13 @@ import { defineConfig } from 'sanity';
 import { deskTool } from 'sanity/desk';
 import { visionTool } from '@sanity/vision';
 import { schemaTypes } from './schemas';
+import { myTheme } from './theme';
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
 
 export default defineConfig({
-  basePath: "/studio",
+  basePath: '/studio',
   name: 'Blog_Content_Studio',
   title: 'Blog Content Studio',
 
@@ -19,4 +20,5 @@ export default defineConfig({
   schema: {
     types: schemaTypes,
   },
+  theme: myTheme,
 });
