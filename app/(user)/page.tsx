b1c23fa639ba98599ffb0edeb7e0d2c5a1f18d27
Filip Gurak <filip.gurak999@gmail.com>
2022-12-19 21:32:20 +0100

    preview mode

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,8 +1,39 @@
-function HomePage() {
+import { previewData } from 'next/headers';
+import { groq } from 'next-sanity';
+import { client } from '../../lib/sanity.client';
+import { PreviewSuspense } from 'next-sanity/preview';
+
+const query = groq`
+*[_type=='post'] {
+  ...,
+  author->,
+  categories->[]
+} | order(_createdAt desc)
+`;
+
+async function HomePage() {
+  if (previewData()) {
+    return (
+      <PreviewSuspense
+        fallback={
+          <div role='status'>
+            <p className='text-center text-lg animate-pulse text-[#f7ab0a]'>Loading Preview Data...</p>
+          </div>
+        }
+      >
+        
+      </PreviewSuspense>
+    );
+  }
+
+  const posts = await client.fetch(query);
+  console.log(posts);
+
   return (
     <div>
+      <h1>Not in preview mode</h1>
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
